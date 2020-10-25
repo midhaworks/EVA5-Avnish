4688818c412f472d253a0014ffa94e4865248020
Avnish Midha <avnishbm@gmail.com>
2020-10-25 23:31:41 +0530

    updated detect.py for weight file name change

--- S13_YoloV34/YoloV3/detect.py
+++ S13_YoloV34/YoloV3/detect.py
@@ -165,10 +165,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--cfg', type=str, default='cfg/yolov3-custom.cfg', help='*.cfg path')
     parser.add_argument('--names', type=str, default='data/customdata/custom.names', help='*.names path')
-    parser.add_argument('--weights', type=str, default='weights/last.pt', help='weights path')
+    parser.add_argument('--weights', type=str, default='weights/best.pt', help='weights path')
     parser.add_argument('--source', type=str, default='data/customdata/images', help='source')  # input file/folder, 0 for webcam
     parser.add_argument('--output', type=str, default='output', help='output folder')  # output folder
-    parser.add_argument('--img-size', type=int, default=512, help='inference size (pixels)')
+    parser.add_argument('--img-size', type=int, default=1080, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.6, help='IOU threshold for NMS')
     parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
